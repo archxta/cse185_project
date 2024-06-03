2c1080f7b3cca61a7967a6abbae5eb3ee34d52ad
kimhdtran10 <kimhdtran@gmail.com>
2024-06-02 20:19:05 -0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,39 +1,19 @@
-from Bio import SeqIO
 import matplotlib.pyplot as plt
 
-# Define the path to the directory containing the variant FASTA files
-fasta_directory = 'data/'
+# Define the sequences
+sequence1 = "AACTTCATCCACGTTCACCTTGCCCCACAGGGCAGTAACGGCAGACTTCTCCACAGGAGTC"
+sequence2 = "AACTTCATCCACGTTCACCTTGCCCCACAGGGCAGTAACGGCAGACTTCTCCTCAGGAGTC"
 
-variant_files = ['variant1.fasta', 'variant2.fasta', 'variant3.fasta']
-variant_sequences = {}
+# Define x coordinates for each nucleotide
+x1 = range(len(sequence1))
+x2 = range(len(sequence2))
 
-# Read variant FASTA files
-for variant_file in variant_files:
-    file_path = fasta_directory + variant_file
-    with open(file_path, 'r') as fasta_file:
-        for record in SeqIO.parse(fasta_file, 'fasta'):
-            variant_sequences[variant_file] = str(record.seq)
-
-# Identify variants and extract variant information
-variant_positions = {}
-for variant_file, sequence in variant_sequences.items():
-    variant_positions[variant_file] = []
-    for i, base in enumerate(sequence):
-        # Check if base differs from reference (assumed reference is first sequence)
-        if base != variant_sequences[variant_files[0]][i]:
-            variant_positions[variant_file].append(i)
-
-# Plot data
-plt.figure(figsize=(10, 6))
-for variant_file, positions in variant_positions.items():
-    allele_frequencies = [len(positions) / len(variant_sequences[variant_file])] * len(positions)
-    plt.scatter(positions, allele_frequencies, label=variant_file)
-
-plt.xlabel('Variant Position')
-plt.ylabel('Allele Frequency')
-plt.title('Allele Frequencies of Variants')
+# Plotting
+plt.figure(figsize=(15, 2))
+plt.scatter(x1, [1] * len(x1), c='blue', label='Sequence 1', s=100)
+plt.scatter(x2, [2] * len(x2), c='red', label='Sequence 2', s=100)
+plt.yticks([])
+plt.xlabel('Position')
+plt.title('Sequence Comparison')
 plt.legend()
-plt.grid(True)
-plt.savefig('allele_frequency_plot.png')
 plt.show()
-
