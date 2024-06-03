bf3aa535456acf48b5ac11f6a59c448cca5bde2a
kimhdtran10 <kimhdtran@gmail.com>
2024-06-02 19:59:43 -0700

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Example data from your VCF file
+variant_positions = [53]
+allele_frequencies = [0.5]  # Assuming a heterozygous variant with 50% allele frequency
+
+# Plotting
+plt.figure(figsize=(8, 6))
+plt.bar(variant_positions, allele_frequencies, color='blue', alpha=0.5)
+plt.xlabel('Variant Position')
+plt.ylabel('Allele Frequency')
+plt.title('Allele Frequencies of Variants')
+plt.xticks(variant_positions)
+plt.show()
