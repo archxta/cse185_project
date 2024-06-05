5e88d13493ae6c809b43b5ade4d97d359d5fc3c6
archxta <165825178+archxta@users.noreply.github.com>
2024-06-04 23:02:03 -0700

    Delete src directory

--- src/bam_indexing.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def index_bam_file(bam_file):
-    os.system(f'samtools index {bam_file}')
-    print(f"Indexed {bam_file}")
--- src/indexing.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def index_reference_genome(reference_fasta):
-    os.system(f'bwa index {reference_fasta}')
-    os.system(f'samtools faidx {reference_fasta}')
-    print(f"Indexed {reference_fasta}")
